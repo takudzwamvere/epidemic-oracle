fce6a664de6e64dfd15d88a334a749b858801ace
Takudzwa Mvere <mveretakudzwa@proton.me>
2025-10-16 14:25:19 +0200

    user roles

---
+++ components/layout/default/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const defaultFooter = () => {
+  return (
+    <div>defaultFooter</div>
+  )
+}
+
+export default defaultFooter
---
+++ components/layout/default/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const defaultHeader = () => {
+  return (
+    <div>defaultHeader</div>
+  )
+}
+
+export default defaultHeader
