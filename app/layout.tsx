8824ff755f4038ada2c7f910e64abf938f5eb5aa
Takudzwa Mvere <mveretakudzwa@proton.me>
2026-05-31 22:25:00 +0200

    umami

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({
@@ -20,9 +21,15 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${inter.className} antialiased min-h-svh bg-gray-50`}>
         {children}
+        <Script 
+          defer 
+          src="https://analytics.takudzwamvere.com/script.js" 
+          data-website-id="12f305fd-b362-40e3-9f02-103ee44165a5" 
+        />
       </body>
     </html>
   );
 }
+
 
 
