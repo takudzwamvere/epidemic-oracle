70a61524aecf76e2e3d88589060edeb579159fa6
Takudzwa Mvere <mveretakudzwa@proton.me>
2026-07-20 23:55:49 +0200

    docs: Document cn class merging utility

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,11 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Combines multiple Tailwind CSS class strings and conditionally merges conflicting classes.
+ * @param inputs List of class names or conditional class values.
+ * @returns Merged className string.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
