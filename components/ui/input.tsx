3092ab11bc24754607378e162438aa52e6a48327
Takudzwa Mvere <mveretakudzwa@proton.me>
2026-07-21 15:54:55 +0200

    docs: Document Input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -2,6 +2,9 @@ import * as React from "react"
 
 import { cn } from "@/lib/utils"
 
+/**
+ * Styled text input component with custom focus states, file upload styling, and accessibility support.
+ */
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
     <input
