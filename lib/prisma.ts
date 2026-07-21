dd61b14c1fde15266a257855afb02dbcd700a9c4
Takudzwa Mvere <mveretakudzwa@proton.me>
2026-07-21 15:53:10 +0200

    docs: Document prisma singleton instance

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,9 @@ import { PrismaClient } from '../app/generated/prisma';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
+/**
+ * Singleton instance of PrismaClient to prevent multiple connections in development mode.
+ */
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
